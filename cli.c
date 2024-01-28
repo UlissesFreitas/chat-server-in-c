d84ef1f873395491d9c709afef18a2d89f7561cc
UlissesFreitas <rulisso@DESKTOP-0I0BBAM>
2024-01-28 10:40:14 -0300

    terminado

--- cli.c
+++ cli.c
@@ -1,6 +1,9 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <poll.h>
+#include <unistd.h>
+
 
 int main(){
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -11,36 +14,34 @@ int main(){
 		0
 	};
 
-	bind(sockfd, &addr, sizeof(address));
-
-	listen(sockfd, 10);
-
-	int clientfd =  accept(sockfd, 0, 0);
+	connect(sockfd,(struct sockaddr *) &address, sizeof(address));
 
 	// stdin - 0
-	struct pollfd fds[2] {
+	struct pollfd fds[2] = {
 	  {
 		0,
 		POLLIN,
 		0
 	  },
 	  {
-		clientfd,
+		sockfd,
 		POLLIN,
 		0
 	  }
 	};
 
 	for (;;){
-		char buffer[256] = {0}
+		char buffer[256] = {0};
 
 		poll(fds, 2, 50000);
 
 		if (fds[0].revents & POLLIN){
 			read(0, buffer, 255);
-			send(clientfd, buffer, 255, 0);
-		} else if (fds[1].revents & POLLIN{
-			recv(clientfd, buffer, 255, 0);
+			send(sockfd, buffer, 255, 0);
+		} else if (fds[1].revents & POLLIN){
+			if ( recv(sockfd, buffer, 255, 0) == 0){
+				return 0;
+			}
 			printf("%s\n", buffer );
 		}
 	}
